e9f36b5d19a3c7e9ab50c3793b41d02eed52f362
Jeovana Santos <114677572+jeovanasantos@users.noreply.github.com>
2023-10-04 15:00:04 +0000

    Animação CSS do card

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
+      <div className= "card">
         <h2>Rick Sanchez</h2>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
       </div>
